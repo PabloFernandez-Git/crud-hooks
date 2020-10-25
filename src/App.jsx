44353aad3161f40babc6832de691644cad07960a
Pablo <pablo.ezequiel.fernandez@gmail.com>
2020-10-25 11:18:11 -0300

    Eliminar Tareas

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,11 @@ function App() {
     setTarea('')
   }
 
+  const eliminarTarea = (id) => {
+    const arrayFiltrado = tareas.filter(item => item.id !== id)
+    setTareas(arrayFiltrado)
+  }
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD simple</h1>
@@ -35,8 +40,17 @@ function App() {
               tareas.map(item => (
                 <li className="list-group-item" key={item.id}>
                   <span className="lead">{item.nombreTarea}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                  <button 
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={ () => eliminarTarea(item.id) }
+                  >
+                    Eliminar
+                  </button>
+                  <button 
+                    className="btn btn-warning btn-sm float-right"
+                  >
+                    Editar
+                  </button>
                 </li>
               ))
             }
